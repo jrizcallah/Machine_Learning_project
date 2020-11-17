e00029459adbaf0761d1aaaebac76c2ed4d27b96
John Rizcallah <johnrizcallah@Gabrielles-iMac.local>
2020-11-17 09:18:19 -0600

    initialize repository for redwine_quality project

---
+++ redwine_quality/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the final project for a machine learning class at WTAMU in Fall 2020. In it, the features which affect the quality of red wine are explored and a model that predicts wine quality based on those features is created.',
+    author='John Rizcallah',
+    license='',
+)
